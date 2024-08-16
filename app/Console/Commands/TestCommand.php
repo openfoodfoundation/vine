7e466f26d0637f07628814894a97731e4ce34f85
ok200paul <ok200paul@users.noreply.github.com>
2024-08-16 04:55:21 +0000

    Lint code

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -2,12 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Team;
-use App\Models\User;
-use App\Models\Voucher;
-use App\Models\VoucherSet;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Storage;
 
 class TestCommand extends Command
 {
@@ -28,9 +23,5 @@ class TestCommand extends Command
     /**
      * Execute the console command.
      */
-    public function handle()
-    {
-
-
-    }
+    public function handle() {}
 }
