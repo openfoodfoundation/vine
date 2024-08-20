2882d8ad7198ff6c54509710bb0884d9dbc3a44e
Paul Grimes <paul@ok200.net>
2024-08-20 20:53:05 +1000

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,8 @@
 <?php
 
-Schedule::command('app:dispatch-collate-system-statistics-job')
-    ->dailyAt('23:55')
-    ->timezone('Australia/Melbourne');
+use App\Console\Commands\DispatchCollateSystemStatisticsJobCommand;
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command(DispatchCollateSystemStatisticsJobCommand::class)
+        ->dailyAt('23:55')
+        ->timezone('Australia/Melbourne');
