4335733862a83c47b98a989287348fd76ea728aa
Paul Grimes <paul@ok200.net>
2024-08-21 12:50:19 +1000

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ \Illuminate\Support\Facades\URL::to('/img/ofn-logo.png') }}" class="logo" alt="{{ config('app.name') }} Logo">
+<img src="{{ \Illuminate\Support\Facades\URL::to('/img/ofn-logo.png') }}" class="" alt="{{ config('app.name') }} Logo">
 </a>
 </td>
 </tr>
