e7bdbc8e1b33b4028602f3cdc4b9985375525bcd
ok200paul <ok200paul@users.noreply.github.com>
2024-09-08 23:47:23 +0000

    Lint code

--- app/Models/VoucherSet.php
+++ app/Models/VoucherSet.php
@@ -6,9 +6,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class VoucherSet extends Model
 {
@@ -33,7 +31,6 @@ class VoucherSet extends Model
     {
         return $this->belongsTo(Team::class, 'allocated_to_service_team_id', 'id');
     }
-
 
     public function voucherSetMerchantTeams(): HasMany
     {
