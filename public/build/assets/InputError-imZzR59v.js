73211f720122825a3ec0dd9b554ca4e0e95d712b
ok200manami <manami@ok200.net>
2024-09-10 10:56:57 +1000

    WIP

--- public/build/assets/InputError-imZzR59v.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/InputError-CtaVr7j8.js
-import{p as s,B as t,a,b as r,e as o,t as c}from"./app-CFlY96qh.js";const n={class:"text-sm text-red-600"},l={__name:"InputError",props:{message:{type:String}},setup(e){return(i,m)=>s((a(),r("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{l as _};
-========
-import{p as s,G as t,a,b as r,e as o,t as c}from"./app-BmSBEPLV.js";const n={class:"text-sm text-red-600"},l={__name:"InputError",props:{message:{type:String}},setup(e){return(i,m)=>s((a(),r("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{l as _};
->>>>>>>> develop:public/build/assets/InputError-imZzR59v.js
--- public/build/assets/InputLabel-BNUlAfJX.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/InputLabel-DMH_e1pT.js
-import{a as e,b as t,t as o,z as n}from"./app-CFlY96qh.js";const l={class:"block font-medium text-sm text-gray-700"},r={key:0},c={key:1},p={__name:"InputLabel",props:{value:{type:String}},setup(s){return(a,_)=>(e(),t("label",l,[s.value?(e(),t("span",r,o(s.value),1)):(e(),t("span",c,[n(a.$slots,"default")]))]))}};export{p as _};
-========
-import{a as e,b as t,t as o,E as n}from"./app-BmSBEPLV.js";const l={class:"block font-medium text-sm text-gray-700"},r={key:0},c={key:1},p={__name:"InputLabel",props:{value:{type:String}},setup(s){return(a,_)=>(e(),t("label",l,[s.value?(e(),t("span",r,o(s.value),1)):(e(),t("span",c,[n(a.$slots,"default")]))]))}};export{p as _};
->>>>>>>> develop:public/build/assets/InputLabel-BNUlAfJX.js
