88ced66d825eafa6e0259edc8b5bfdaf94cf7d09
ok200lyndon <lyndon@ok200.net>
2024-09-12 09:25:32 +1000

    Update VoucherSetService.php

--- app/Services/VoucherSetService.php
+++ app/Services/VoucherSetService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Voucher;
 use App\Models\VoucherRedemption;
 use App\Models\VoucherSet;
 
@@ -24,7 +25,7 @@ class VoucherSetService
      */
     public static function collateVoucherSetAggregates(VoucherSet $voucherSet): void
     {
-        $voucherSet->num_vouchers              = VoucherSet::where('voucher_set_id', $voucherSet->id)->count();
+        $voucherSet->num_vouchers              = Voucher::where('voucher_set_id', $voucherSet->id)->count();
         $voucherSet->num_voucher_redemptions   = VoucherRedemption::where('voucher_set_id', $voucherSet->id)->count();
         $voucherSet->last_redemption_at        = VoucherRedemption::where('voucher_set_id', $voucherSet->id)->max('created_at');
         $voucherSet->total_set_value_remaining = self::calculateVoucherSetValueRemaining($voucherSet);
