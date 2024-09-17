5d319ccb469bcca01105a1ec7aaff1f46f7d66b9
Paul Grimes <paul@ok200.net>
2024-09-17 15:31:29 +1000

    Prod build

--- database/migrations/2024_09_17_052637_update_teams_table_with_country_id.php
+++ database/migrations/2024_09_17_052637_update_teams_table_with_country_id.php
@@ -4,20 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('teams', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->index('t_n');
+       Schema::table('teams', function (Blueprint $table) {
+           $table->unsignedSmallInteger('country_id')->after('name')->default(14)->index('t_ci');
+       });
 
-            $table->timestamps();
-            $table->softDeletes();
-        });
+
     }
 
     /**
@@ -25,6 +23,6 @@ return new class() extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('teams');
+       Schema::dropColumns('teams', 'country_id');
     }
 };
