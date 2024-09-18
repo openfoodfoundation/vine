95cfb446fbd8ffebcce5934354beffb83bd48f9b
ok200lyndon <lyndon@ok200.net>
2024-09-18 16:29:21 +1000

    Update VoucherSetService.php

--- app/Services/VoucherSetService.php
+++ app/Services/VoucherSetService.php
@@ -9,6 +9,28 @@ use App\Models\VoucherSet;
 class VoucherSetService
 {
     /**
+     * Determine if a voucher set's JSON is valid, and matched its value
+     *
+     * @param VoucherSet $voucherSet
+     *
+     * @return mixed
+     */
+    public static function validateVoucherSetDenominations(VoucherSet $voucherSet): bool
+    {
+        $denominationArray = json_decode($voucherSet->denomination_json, true);
+
+        $totalValue = 0;
+
+        if (!is_null($denominationArray)) {
+            foreach ($denominationArray as $denominationListing) {
+                $totalValue += ($denominationListing['value'] * $denominationListing['number']);
+            }
+        }
+
+        return $totalValue == $voucherSet->total_set_value;
+    }
+
+    /**
      * @param VoucherSet $voucherSet
      *
      * @return int
@@ -25,6 +47,7 @@ class VoucherSetService
      */
     public static function collateVoucherSetAggregates(VoucherSet $voucherSet): void
     {
+        $voucherSet->is_denomination_valid = self::validateVoucherSetDenominations($voucherSet);
         $voucherSet->num_vouchers              = Voucher::where('voucher_set_id', $voucherSet->id)->count();
         $voucherSet->total_set_value           = Voucher::where('voucher_set_id', $voucherSet->id)->sum('voucher_value_original');
         $voucherSet->num_voucher_redemptions   = VoucherRedemption::where('voucher_set_id', $voucherSet->id)->count();
