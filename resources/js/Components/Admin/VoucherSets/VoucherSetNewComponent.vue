4b1c6c6ea3fcc47ac0a76e52f17d813533e0af97
Paul Grimes <paul@ok200.net>
2024-09-24 13:25:10 +1000

    Update VoucherSetNewComponent.vue

--- resources/js/Components/Admin/VoucherSets/VoucherSetNewComponent.vue
+++ resources/js/Components/Admin/VoucherSets/VoucherSetNewComponent.vue
@@ -885,11 +885,11 @@ watch(serviceTeamSearchQuery, () => {
                         </svg>
 
                         <svg v-else
-                             class="size-6 text-orange-500 fill-orange-100" fill="none" stroke="currentColor"
+                             class="size-6 text-red-500 fill-red-100" fill="none" stroke="currentColor"
                              stroke-width="1.5"
                              viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path
-                                d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
+                                d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
                                 stroke-linecap="round"
                                 stroke-linejoin="round"/>
                         </svg>
