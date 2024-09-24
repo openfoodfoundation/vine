376b5f1bd297a4836b8cc8bd4d05010374a84784
Paul Grimes <paul@ok200.net>
2024-09-24 13:06:20 +1000

    Updated email to conditionally attach voucher card

--- app/Notifications/VoucherBeneficiaryDistributions/BeneficiaryVoucherDistributionEmail.php
+++ app/Notifications/VoucherBeneficiaryDistributions/BeneficiaryVoucherDistributionEmail.php
@@ -2,10 +2,12 @@
 
 namespace App\Notifications\VoucherBeneficiaryDistributions;
 
+use App\Models\Voucher;
 use App\Models\VoucherBeneficiaryDistribution;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Storage;
 
 class BeneficiaryVoucherDistributionEmail extends Notification
 {
@@ -16,7 +18,9 @@ class BeneficiaryVoucherDistributionEmail extends Notification
      *
      * @param VoucherBeneficiaryDistribution $voucherBeneficiaryDistribution
      */
-    public function __construct(public VoucherBeneficiaryDistribution $voucherBeneficiaryDistribution) {}
+    public function __construct(public VoucherBeneficiaryDistribution $voucherBeneficiaryDistribution)
+    {
+    }
 
     /**
      * Get the notification's delivery channels.
@@ -39,11 +43,37 @@ class BeneficiaryVoucherDistributionEmail extends Notification
      */
     public function toMail(object $notifiable): MailMessage
     {
-        return (new MailMessage())
-            ->line('Your voucher has been distributed to you.')
-            ->line('You can access it by scanning the QR code below.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
+
+        $voucher = Voucher::find($this->voucherBeneficiaryDistribution->voucher_id);
+
+        $mailMessage = (new MailMessage())
+            ->subject('VINE: Your Voucher #' . $this->voucherBeneficiaryDistribution->voucher_id)
+            ->line('When making an online purchase at a supported merchant, please use the following discount code:')
+            ->line($voucher->voucher_short_code);
+
+        $voucherTemplateStoragePath = '/voucher-sets/' .
+            $this->voucherBeneficiaryDistribution->voucher_set_id . '/vouchers/individual/' .
+            $this->voucherBeneficiaryDistribution->voucher_id . '/branded/voucher-branded.png';
+
+        $brandedVoucherTemplateExists = Storage::exists($voucherTemplateStoragePath);
+        if($brandedVoucherTemplateExists)
+        {
+            $brandedVoucherTemplateContents = Storage::get($voucherTemplateStoragePath);
+
+            $mailMessage = $mailMessage->line('Please see attached for your VINE voucher. ')
+                                       ->attachData(
+                data   : $brandedVoucherTemplateContents,
+                name   : 'voucher-' . $this->voucherBeneficiaryDistribution->voucher_id . '.png',
+                options: [
+                             'mime' => 'image/png',
+                         ]
+            );
+        }
+
+        $mailMessage = $mailMessage->line('Thank you for using our application!');
+
+
+        return $mailMessage;
     }
 
     /**
