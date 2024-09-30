3120baf1f27fbc4c95a6afcb60d7097b9a3c3cbb
Paul Grimes <paul@ok200.net>
2024-09-30 10:06:15 +1000

    Create 2024_09_30_000442_update_voucher_sets_add_currency_country_id.php

---
+++ database/migrations/2024_09_30_000442_update_voucher_sets_add_currency_country_id.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('voucher_sets', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('currency_country_id')
+                  ->nullable()
+                  ->index('vs_cci')
+                  ->after('voucher_template_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+
+        Schema::table('voucher_sets', function (Blueprint $table) {
+            $table->dropColumn('currency_country_id');
+        });
+    }
+};
